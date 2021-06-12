a19299132c233eef37ea71387b54f47ddeb31880
wbwd <77216609+kmcpherson49@users.noreply.github.com>
2021-06-12 10:05:44 -0700

    add gallery

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,20 +1,18 @@
-import React from "react";
-import { render, cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/extend-expect";
-import About from "..";
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
 
 afterEach(cleanup);
 
-describe("About component", () => {
-  // First Test
-  it("renders", () => {
+describe('About component renders', () => {
+  it('renders', () => {
     render(<About />);
   });
-  //Second Test
-  it("matches snapshot DOM node structure", () => {
-    //render About
+  
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<About />);
+    
+    expect(asFragment()).toMatchSnapshot();
   });
-});
-
-const { asFragment } = render(<About />);
-expect(asFragment()).toMatchSnapshot();
+})
